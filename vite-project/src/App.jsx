e46c4cf50314f8829ef7f4a530897d65d2b82c1f
sezerz1121 <85015473+sezerz1121@users.noreply.github.com>
2024-08-24 00:05:47 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import Links from './Component/Links';
+import Links from './component/Links';
 function App() {
   const [file, setFile] = useState(null);
 
