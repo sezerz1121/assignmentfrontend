8842ac54277d6def463ba52c68a0e5c89007f043
sezerz1121 <85015473+sezerz1121@users.noreply.github.com>
2024-08-24 00:19:47 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Upload from './upload';
+import Upload from './Upload';
 function App() {
   
  
